4f5bf468a4034a7964cea6ff8864234d0000772a
z1the3 <1813783831@qq.com>
2023-09-23 00:41:12 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HelloWorld } from './pages/HelloWorld'
+function App() {
+
+  return (
+    <>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HelloWorld/index.tsx
@@ -0,0 +1,3 @@
+export const HelloWorld = () => {
+    return <div>hello,world!</div>
+}
